9d9242c501c059267108efc7f5a9b268c9d3645f
jonathan cattelain <jonathan.cattelain@gmail.com>
2023-11-15 15:19:38 +0100

    huhu

--- main.py
+++ main.py
@@ -5,8 +5,10 @@
 
 
 def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == "__main__":
+    print_hi("PyCharm")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
